e4d9611e2216a9527a897dc37f0dbcb918343fff
HitoKawiswara <hitokawiswara@gmail.com>
2025-11-23 12:14:17 +0700

    Commit No. 4: Complete Bonus Assignment (Add Update Timestamp)

--- app/src/main/java/com/example/lab_week_10/viewmodels/TotalViewModel.kt
+++ app/src/main/java/com/example/lab_week_10/viewmodels/TotalViewModel.kt
@@ -6,23 +6,18 @@ import androidx.lifecycle.ViewModel
 
 class TotalViewModel : ViewModel() {
 
-    // Declare the LiveData object
     private val _total = MutableLiveData<Int>()
     val total: LiveData<Int> = _total
 
-    // Initialize the LiveData object
     init {
-        // postValue can be used from main thread or background thread
         _total.postValue(0)
     }
 
-    // Increment the total value
     fun incrementTotal() {
         _total.postValue(_total.value?.plus(1))
     }
 
-    //Set new total value
-    fun setTotal(newTotal: Int) {
-        _total.postValue(newTotal)
+    fun setTotal(total: Int) {
+        _total.postValue(total)
     }
 }
